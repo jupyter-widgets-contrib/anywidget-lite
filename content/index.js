a3c3f5b351a5b6d7d919f7d7c5d316c9dd89b056
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-02-26 21:06:09 +0100

    Add index files (#2)
    * Add index files
    
    * update notebook
    
    * edit readme

---
+++ content/index.js
@@ -0,0 +1,16 @@
+function render({ model, el }) {
+  let getCount = () => model.get("count");
+  let button = document.createElement("button");
+  button.classList.add("counter-button");
+  button.innerHTML = `count is ${getCount()}`;
+  button.addEventListener("click", () => {
+    model.set("count", getCount() + 1);
+    model.save_changes();
+  });
+  model.on("change:count", () => {
+    button.innerHTML = `COUNT is ${getCount()}`;
+  });
+  el.appendChild(button);
+}
+
+export default { render };
